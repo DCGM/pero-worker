57e5314e596a7dcde0227e474385f75d831ecff7
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2022-10-26 14:07:41 -0500

    Adding tarfile member sanitization to extractall() (#7)

--- worker/worker.py
+++ worker/worker.py
@@ -1018,7 +1018,26 @@ class Worker(object):
         """
         if tarfile.is_tarfile(archive):
             with tarfile.open(archive, 'r') as arch:
-                arch.extractall(path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(arch, path)
         else:
             with zipfile.ZipFile(archive, 'r') as arch:
                 arch.extractall(path)
